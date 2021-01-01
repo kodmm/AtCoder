ec0065a6330d9de2be5b3706eff59b3e4b9e5245
kodmm <53547701+kodmm@users.noreply.github.com>
2021-01-02 02:19:08 +0900

    Placing Marbles

---
+++ AtCorderBeginnersSelection/ABC081A.py
@@ -0,0 +1,10 @@
+# ３文字の整数の入力
+s = str(input())
+# listにする
+list_s = list(s)
+# integer型のlistにする
+list_s = [int(i) for i in list_s]
+# list_sの値が`1`が格納されている個数を求める
+result = list_s.count(1)
+
+print("{}".format(result))
